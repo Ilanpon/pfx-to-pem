669518f3c6708f3a65b7c77128186702a8586fa6
ilanponimansky <31193909+ilanponimansky@users.noreply.github.com>
2017-08-27 15:07:56 -1000

    Update pfxtopem.py

--- pfxtopem.py
+++ pfxtopem.py
@@ -2,42 +2,62 @@ from OpenSSL import crypto
 import argparse
 from os import path
 
-parser = argparse.ArgumentParser(prog='PFXtoPEM', description='Converts MS stuff to the superior format')
-parser.add_argument('-password', type=str, nargs='?', default='', required=False, help='The password used for the cert')
-parser.add_argument('-file', type=str, nargs='?', default='', required=True, help='The path to pfx file')
+
+parser = argparse.ArgumentParser(
+    prog='PFXtoPEM',
+    description='Converts MS stuff to the superior format and uploads it to Vault, if you specify the URL'
+)
+
+parser.add_argument('-password', type=str, nargs='?', default='',
+                    help='The password used for the cert')
+
+parser.add_argument('-file', type=str, nargs='?', default='', required=True,
+                    help='The path to pfx file')
+
+
 args = parser.parse_args()
 
 pfx_password = args.password
 pfx_file = args.file
 
 
-def convertpfx(password, file):
-    # open the input file and extract data
-    with open(file, 'rb') as pfx:
-        pem_converter = crypto.load_pkcs12(pfx.read(), password)
-        pem_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, pem_converter.get_privatekey())
-        pem_cert = crypto.dump_certificate(crypto.FILETYPE_PEM, pem_converter.get_certificate())
+class PemFormat:
+    def convertpfx(self, password, file):
+        # open the input file and extract data
+        print(f'Converting {file} to PEM \n')
 
-    # write key
-    pem_file = path.splitext(file)[0]
-    with open(pem_file + '-key.pem', 'wb') as file:
-        file.write(pem_key)
+        with open(file, 'rb') as pfx:
+            pem_converter = crypto.load_pkcs12(pfx.read(), password)
+            self.pem_key = crypto.dump_privatekey(crypto.FILETYPE_PEM, pem_converter.get_privatekey())
+            self.pem_cert = crypto.dump_certificate(crypto.FILETYPE_PEM, pem_converter.get_certificate())
 
-    # write cert
-    with open(pem_file + '-cert.pem', 'wb') as file:
-        file.write(pem_cert)
+        # write key
+        pem_file = path.splitext(file)[0]
+        with open(pem_file + '-key.pem', 'wb') as file:
+            print(f'Writing key to {file.name}')
+            file.write(self.pem_key)
 
-    # write ca, if any
-    with open(pem_file + '-ca.pem', 'wb') as file:
-        ca = pem_converter.get_ca_certificates()
-        if ca is not None:
-            for cert in ca:
-                file.write(crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+        # write cert
+        with open(pem_file + '-cert.pem', 'wb') as file:
+            print(f'Writing cert to {file.name}')
+            file.write(self.pem_cert)
+
+        # write ca, if any
+        with open(pem_file + '-ca.pem', 'wb') as file:
+            ca = pem_converter.get_ca_certificates()
+            if ca is not None:
+                print(f'Writing ca data to {file.name} \n')
+                for cert in ca:
+                    self.pem_ca = crypto.dump_certificate(crypto.FILETYPE_PEM, cert)
+                    file.write(self.pem_ca)
+
 
 if __name__ == "__main__":
-    print(f'Converting {pfx_file} to PEM')
-    convertpfx(pfx_password, pfx_file)
-    
+    converter = PemFormat()
+    converter.convertpfx(pfx_password, pfx_file)
+
+
+
 
 
 
